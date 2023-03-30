51e6a9ddb19021bc34f4db267ea3fa8c262dcee5
Sebas <sbriones284@gmail.com>
2023-03-30 11:14:43 -0600

    finalizacion clase 11

--- Tienda/src/main/java/com/Tienda/controller/ClienteController.java
+++ Tienda/src/main/java/com/Tienda/controller/ClienteController.java
@@ -22,11 +22,16 @@ public class ClienteController {
 
     @GetMapping("/cliente/listado")
     public String inicio(Model model) {
-
-        var clientes = clienteService.getClientes();
-        //var clientes = clienteService.getClienteNombre("Ana");
-
-        model.addAttribute("clientes", clientes);
+        var clientes=clienteService.getClientes();
+        
+        var limiteTotal=0;
+        for (var c: clientes) {
+            limiteTotal+=c.getCredito().getLimite();
+        }
+        model.addAttribute("limiteTotal",limiteTotal);
+        model.addAttribute("totalClientes",clientes.size());
+        
+        model.addAttribute("clientes",clientes);
         return "/cliente/listado";
     }
 
